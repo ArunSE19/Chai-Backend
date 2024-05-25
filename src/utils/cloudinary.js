10f5a9454bdc34176f2d1b2c3eccac01cd7eaa62
ArunSE19 <arunku1feb@gmail.com>
2024-05-25 14:45:37 +0500

    "Cloudinary And Multer Configuration"

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    
+    
+    const uploadOnCloudinary= async (localFilePath)=>{
+        try{
+            if(!localFilePath) return null
+            const response= await cloudinary.uploader.upload(localFilePath,{resource_type:'auto'})
+
+            console.log("File path",response.url);
+            return response;
+        }
+        catch(error){
+            fs.unlink(localFilePath)//this removes localfilepath if upload is not performed successfully
+            return null
+        }
+    }
+
+export {uploadOnCloudinary}
+    
