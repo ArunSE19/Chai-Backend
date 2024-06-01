ec0450547dc94e64c3c84918400491f0198d048c
ArunSE19 <arunku1feb@gmail.com>
2024-06-01 16:51:47 +0500

    "Aggregation Pipelines Done"

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const subscriptionSchema=new mongoose.Schema({
 
-    subscriber:[{
+    subscriber:{
         type: mongoose.Schema.Types.ObjectId,//one who is subscribing
         ref:"User"
-    }],
+    },
     channel:{
         type: mongoose.Schema.Types.ObjectId,//one who is being subscribed
         ref:"User"
