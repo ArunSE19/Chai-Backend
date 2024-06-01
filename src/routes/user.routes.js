b54ad36e5d58a25cde89d581cf8e715ae04bf221
ArunSE19 <arunku1feb@gmail.com>
2024-06-01 17:12:47 +0500

    "Routes Done"

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -32,7 +32,7 @@ router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 
 router.route("/avatarImage").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
 
-router.route("/coverImage").patch(verifyJWT, upload.single("/coverImage"),updateUserCoverImage)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserProfile)
 
